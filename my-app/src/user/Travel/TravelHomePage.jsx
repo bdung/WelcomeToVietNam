a018c065d46cacab20e252fc04c5e7ffc0538dbd
ThaoLang <90896633+ThaoLang@users.noreply.github.com>
2023-08-14 11:33:28 +0700

    fix: fix route navigated from travel homepage to travel detailed page

--- my-app/src/user/Travel/TravelHomePage.jsx
+++ my-app/src/user/Travel/TravelHomePage.jsx
@@ -14,113 +14,139 @@ import BannerSmallPost from "../../components/BannerSmallPost";
 export default function TravelHomePage() {
   const lastestPost = [
     {
+      id: 1,
       url: SonDooongImg,
       title: "Thiên đường bí ẩn trong lòng đất - Sơn Đoòng Việt Nam",
       uploadTime: "03/04/2023",
       timeForRead: "3 phút đọc",
       like: "1130",
       comment: "244",
+      page: "travel",
     },
     {
+      id: 2,
       url: NhaThoImg,
       title: "Nhà thờ Đức Bà - Biểu tượng kiến trúc của Sài Gòn",
       uploadTime: "05/04/2023",
       timeForRead: "4 phút đọc",
       like: "329",
       comment: "50",
+      page: "travel",
     },
     {
+      id: 3,
       url: HoiAnImg,
       title: "Hội An - Nơi giao thoa văn hóa và nét đẹp kiến trúc...",
       uploadTime: "03/04/2023",
       timeForRead: "2 phút đọc",
       like: "420",
       comment: "13",
+      page: "travel",
     },
     {
+      id: 4,
       url: NhaTrangImg,
       title: "Nha Trang - Nơi lý tưởng cho người yêu biển và du lịch ",
       uploadTime: "03/04/2023",
       timeForRead: "2 phút đọc",
       like: "420",
       comment: "13",
+      page: "travel",
     },
     {
+      id: 5,
       url: HaLongImg,
       title: "Vịnh Hạ Long - Nơi gặp gỡ giữa trời đất và biển cả",
       uploadTime: "03/04/2023",
       timeForRead: "2 phút đọc",
       like: "420",
       comment: "13",
+      page: "travel",
     },
     {
+      id: 6,
       url: HoGuomImg,
       title: "Hồ Gươm - Điểm đến không thể bỏ qua khi đến Hà Nội",
       uploadTime: "03/04/2023",
       timeForRead: "2 phút đọc",
       like: "420",
       comment: "13",
+      page: "travel",
     },
   ];
 
   const postsTravel = [
     {
+      id: 7,
       url: "https://as2.ftcdn.net/v2/jpg/02/84/81/79/1000_F_284817904_ox8667zRGi9LQl4ocAGlNoV7kkqImkuQ.jpg",
       title: "Ruộng bậc thang Hà Giang - nét đẹp miền núi...",
       uploadTime: "03/04/2023",
       timeForRead: "3 phút đọc",
       like: "10",
       comment: "2",
+      page: "travel",
     },
     {
+      id: 5,
       url: "https://img.freepik.com/free-photo/peak-bamboo-lijiang-rural-mist_1417-410.jpg?w=996&t=st=1691327697~exp=1691328297~hmac=0a5a31b3d1dc6a46ab5eda0a2c867821df306e7c112c90a950c178a555fe8599",
       title: "Vịnh Hạ Long, một nơi không thể không đến!",
       uploadTime: "05/04/2023",
       timeForRead: "4 phút đọc",
       like: "329",
       comment: "50",
+      page: "travel",
     },
     {
+      id: 8,
       url: "https://img.freepik.com/free-photo/vacation-stone-vietnam-fresh-green-china_1417-1360.jpg?w=996&t=st=1691327741~exp=1691328341~hmac=768bfbc260f7112eb463fa1836647c5a49fb7184ebcb6ba65f754c3aa62d15ec",
       title: `Khám phá những con thác hùng vĩ của Việt Nam `,
       uploadTime: "03/04/2023",
       timeForRead: "2 phút đọc",
       like: "110",
       comment: "24",
+      page: "travel",
     },
   ];
   const postsHighlight = [
     {
+      id: 1,
       url: SonDooongImg,
       title: "Thiên đường bí ẩn trong lòng đất - Sơn Đoòng Việt Nam",
       uploadTime: "03/04/2023",
       timeForRead: "3 phút đọc",
       like: "1130",
       comment: "244",
+      page: "travel",
     },
     {
+      id: 6,
       url: HoGuomImg,
       title: "Hồ Gươm - Điểm đến không thể bỏ qua khi đến Hà Nội",
       uploadTime: "03/04/2023",
       timeForRead: "2 phút đọc",
       like: "420",
       comment: "13",
+      page: "travel",
     },
     {
+      id: 2,
       url: NhaThoImg,
       title: "Nhà thờ Đức Bà - Biểu tượng kiến trúc của Sài Gòn",
       uploadTime: "05/04/2023",
       timeForRead: "4 phút đọc",
       like: "329",
       comment: "50",
+      page: "travel",
     },
     {
+      id: 5,
       url: HaLongImg,
       title: "Vịnh Hạ Long - Nơi gặp gỡ giữa trời đất và biển cả",
       uploadTime: "03/04/2023",
       timeForRead: "2 phút đọc",
       like: "420",
       comment: "13",
+      page: "travel",
     },
   ];
 
@@ -180,7 +206,7 @@ export default function TravelHomePage() {
           {/* <div className="flex justify-around items-center font-poppins mb-40"> */}
           <div className="grid grid-cols-3 md:grid-cols-3 lg:grid-cols-3 gap-5 mr-10 ">
             {postsTravel.map((post, postIndex) => (
-              <BannerSmallPost postInfo={post} key={postIndex} />
+              <BannerSmallPost postInfo={post} key={post.id} />
             ))}
           </div>
         </div>
@@ -218,7 +244,7 @@ export default function TravelHomePage() {
       <div className="pl-2 mt-10 flex items-center justify-center max-w-screen-lg mx-auto relative mb-20">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 mr-10">
           {postsHighlight.map((post, postIndex) => (
-            <SmallPost postInfo={post} key={postIndex} />
+            <SmallPost postInfo={post} key={post.id} />
           ))}
         </div>
         <div className=" absolute right-1 text-4xl text-[#1AA1D5] cursor-pointer">
@@ -238,7 +264,7 @@ export default function TravelHomePage() {
       <div className="flex justify-around items-center font-poppins mb-40">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 w-7/12 pt-10 ">
           {lastestPost.map((post, postIndex) => (
-            <SmallPost postInfo={post} key={postIndex} />
+            <SmallPost postInfo={post} key={post.id} />
           ))}
         </div>
       </div>
